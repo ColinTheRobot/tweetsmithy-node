37828b01611dd0757e37fd4bd74d0c1e420a46e8
Colin Hart <me@colin-hart.com>
2016-03-18 16:16:57 -0400

    add a character count and conditional

--- public/js/output_element.js
+++ public/js/output_element.js
@@ -1,10 +1,22 @@
 'use strict'
 var React = require('react');
+
 const OutputElement = React.createClass({
   render : function() {
     var value = this.props.data
+    var count = 140 - this.props.data.length;
+    var pStyle = {display: 'inline', fontSize: '8px', verticalAlign: 'bottom' }
+
     return (
-        <p>{value}</p>
+      <div>
+        <p className="output"> {value} </p>
+
+        {count !== 140 ? (
+          <p style={pStyle}> {count} </p>
+        ) : (
+          <p style={pStyle}></p>
+        )}
+      </div>
     );
   }
 })
